14f631f7cfdab0b1964e92c545231437c2192383
Kurt Thams <thams@thams.com>
2013-11-18 10:37:08 -0800

    log before notify, not the other way around. What was I thinking?

--- lib/notify_airbrake.rb
+++ lib/notify_airbrake.rb
@@ -9,13 +9,13 @@ module NotifyAirbrake
 
   def notify_airbrake(ex)
     begin
+      Rails.logger.error(ex.backtrace) # Do not put before Toadhopper call. Maybe it needs to go before Toadhopper
       Airbrake.configure do |config|
         Toadhopper(config.api_key).post!(ex)
       end
-      Rails.logger.error(ex.backtrace) # Do not put before Toadhopper call
     rescue Timeout::Error => timeout
       # sometimes Airbrake timesout. Best we can do is log it
-      Rails.logger.error(ex.backtrace)
+      # Rails.logger.error(ex.backtrace) (took this out sinde brying logger before Airbrake)
       Rails.logger.error(timeout.backtrace)
     end
   end
