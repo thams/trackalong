376c9cfe8400637e43f8f8d56a6943d1711c84bf
Kurt Thams <thams@thams.com>
2020-10-01 21:47:18 -0700

    remove all the old Ruby stuff, in prep for new edition in Smalltalk

--- app/controllers/application_controller.rb
+++
@@ -1,5 +0,0 @@
-class ApplicationController < ActionController::Base
-  protect_from_forgery
-  # TODO: Could do this instead of AirBrake
-  # include ExceptionNotifiable
-end
--- config/initializers/airbrake.rb
+++
@@ -1,5 +0,0 @@
-# TODO: gemify this along with the notify_airbrake gem??
-Airbrake.configure do |config|
-  config.api_key = YAML::load(File.open("#{Rails.root}/config/api_keys.yml"))[Rails.env]["airbrake_key"]
-  config.development_environments = ["production", "development", "test"] # Or leave empty for all envs
-end
